0fa5837f5aebcf729db315e7a48a29a05f71f871
Chris Horton <hortoncd@gmail.com>
2017-07-22 20:20:43 -0700

    use interfaces to support string as well as int

--- binarysearch.go
+++ binarysearch.go
@@ -1,22 +1,62 @@
 package binarysearch
 
+type SearchInt struct {
+	List []int
+}
+
+type SearchString struct {
+	List []string
+}
+
 // BinarySearch implements the binary search algorithm on an array of integers
 // assuming that the input array is already sorted
-func BinarySearch(input *[]int, low int, high int, key int) int {
+func (s SearchInt) BinarySearch(low int, high int, key int) int {
+
+	m := mid(low, high)
+	if m < 0 {
+		return -1
+	}
+
+	if s.List[m] == key {
+		return m
+	}
+
+	if key < s.List[m] {
+		return s.BinarySearch(low, m-1, key)
+	} else {
+		return s.BinarySearch(m+1, high, key)
+	}
+
+}
+
+// BinarySearch implements the binary search algorithm on an array of strings
+// assuming that the input array is already sorted
+func (s SearchString) BinarySearch(low int, high int, key string) int {
+
+	m := mid(low, high)
+	if m < 0 {
+		return -1
+	}
+
+	if s.List[m] == key {
+		return m
+	}
+
+	if key < s.List[m] {
+		return s.BinarySearch(low, m-1, key)
+	} else {
+		return s.BinarySearch(m+1, high, key)
+	}
+
+}
+
+// mid tests high/low for validity and returns mid point
+func mid(low int, high int) int {
 
 	if high < low {
 		return -1
 	}
 
-	mid := low + ((high - low) / 2)
+	return low + ((high - low) / 2)
 
-	if (*input)[mid] == key {
-		return mid
-	}
-
-	if key < (*input)[mid] {
-		return BinarySearch(input, low, mid-1, key)
-	} else {
-		return BinarySearch(input, mid+1, high, key)
-	}
 }
--- binarysearch_test.go
+++ binarysearch_test.go
@@ -4,39 +4,45 @@ import (
 	"testing"
 )
 
-type testgroup struct {
-	input []int
+type inttestgroup struct {
+	input SearchInt
 	key   int
 	index int
 }
 
-func testGroupSetup(t *testing.T) []testgroup {
-	tests := []testgroup{
-		{[]int{}, 3, -1},
-		{[]int{1, 2, 3, 4}, 3, 2},
-		{[]int{1, 2, 3, 4}, 7, -1},
-		{[]int{8, 9, 10, 11}, 7, -1},
-		{[]int{8, 10, 9, 11}, 9, -1}, // incorrectly sorted
-		{[]int{3, 5, 8, 10, 12, 15, 18, 20, 20, 50, 60}, 50, 9},
-		{[]int{1, 5, 8, 12, 13}, 8, 2},
-		{[]int{1, 5, 8, 12, 13}, 1, 0},
-		{[]int{1, 5, 8, 12, 13}, 23, -1},
-		{[]int{1, 5, 8, 12, 13}, 1, 0},
-		{[]int{1, 5, 8, 12, 13}, 11, -1},
+type stringtestgroup struct {
+	input SearchString
+	key   string
+	index int
+}
+
+func testGroupSetupInt(t *testing.T) []inttestgroup {
+	tests := []inttestgroup{
+		{SearchInt{[]int{}}, 3, -1},
+		{SearchInt{[]int{1, 2, 3, 4}}, 3, 2},
+		{SearchInt{[]int{1, 2, 3, 4}}, 7, -1},
+		{SearchInt{[]int{8, 9, 10, 11}}, 7, -1},
+		{SearchInt{[]int{8, 10, 9, 11}}, 9, -1}, // incorrectly sorted
+		{SearchInt{[]int{3, 5, 8, 10, 12, 15, 18, 20, 20, 50, 60}}, 50, 9},
+		{SearchInt{[]int{1, 5, 8, 12, 13}}, 8, 2},
+		{SearchInt{[]int{1, 5, 8, 12, 13}}, 1, 0},
+		{SearchInt{[]int{1, 5, 8, 12, 13}}, 23, -1},
+		{SearchInt{[]int{1, 5, 8, 12, 13}}, 1, 0},
+		{SearchInt{[]int{1, 5, 8, 12, 13}}, 11, -1},
 	}
 	var bigarray []int
 	for i := 1; i <= 1000; i++ {
 		bigarray = append(bigarray, i)
 	}
-	bigtest := testgroup{bigarray, 150, 149}
+	bigtest := inttestgroup{SearchInt{bigarray}, 150, 149}
 	tests = append(tests, bigtest)
 
 	return tests
 }
 
-func TestBinarySearch(t *testing.T) {
-	for _, group := range testGroupSetup(t) {
-		ret := BinarySearch(&group.input, 0, len(group.input)-1, group.key)
+func TestBinarySearchInt(t *testing.T) {
+	for _, group := range testGroupSetupInt(t) {
+		ret := group.input.BinarySearch(0, len(group.input.List)-1, group.key)
 		if ret != group.index {
 			t.Error(
 				"For", group.input,
@@ -45,4 +51,35 @@ func TestBinarySearch(t *testing.T) {
 			)
 		}
 	}
+
+}
+
+func testGroupSetupString(t *testing.T) []stringtestgroup {
+	tests := []stringtestgroup{
+		{SearchString{[]string{}}, "blah", -1},
+		{SearchString{[]string{"one", "two", "three", "four"}}, "three", -1},
+		{SearchString{[]string{"one", "three", "two", "four"}}, "four", -1},
+		{SearchString{[]string{"four", "one", "three", "two"}}, "four", 0},
+		{SearchString{[]string{"four", "one", "three", "two"}}, "one", 1},
+		{SearchString{[]string{"four", "one", "three", "two"}}, "three", 2},
+		{SearchString{[]string{"four", "one", "three", "two"}}, "two", 3},
+		{SearchString{[]string{"a", "n", "w", "z"}}, "n", 1},
+		{SearchString{[]string{"a", "n", "w", "z"}}, "z", 3},
+	}
+
+	return tests
+}
+
+func TestBinarySearchString(t *testing.T) {
+	for _, group := range testGroupSetupString(t) {
+		ret := group.input.BinarySearch(0, len(group.input.List)-1, group.key)
+		if ret != group.index {
+			t.Error(
+				"For", group.input,
+				"expected", group.index,
+				"got", ret,
+			)
+		}
+	}
+
 }
