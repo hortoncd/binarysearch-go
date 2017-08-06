45e4cd1e89635470d4600a8bc7bd0b37a6b05d05
Chris Horton <hortoncd@gmail.com>
2017-08-05 19:11:05 -0700

    use interface for type support
    Previously added string support, but didn't quite get around
    to the interface to minimize the differences in calling
    externally.

--- binarysearch.go
+++ binarysearch.go
@@ -2,15 +2,32 @@ package binarysearch
 
 type SearchInt struct {
 	List []int
+	Key  int
 }
 
 type SearchString struct {
 	List []string
+	Key  string
+}
+
+type Searchable interface {
+	binarySearch() int
+}
+
+// BinarySearch takes and interface and calls the search for the specific
+// data type of the arg, in order to support strings with ints
+func BinarySearch(s Searchable) int {
+	return s.binarySearch()
+}
+
+// binarySearch handles calling the int-specific BinarySearch with args
+func (s SearchInt) binarySearch() int {
+	return s.doBinarySearch(0, len(s.List)-1, s.Key)
 }
 
 // BinarySearch implements the binary search algorithm on an array of integers
 // assuming that the input array is already sorted
-func (s SearchInt) BinarySearch(low int, high int, key int) int {
+func (s SearchInt) doBinarySearch(low int, high int, key int) int {
 
 	m := mid(low, high)
 	if m < 0 {
@@ -22,16 +39,21 @@ func (s SearchInt) BinarySearch(low int, high int, key int) int {
 	}
 
 	if key < s.List[m] {
-		return s.BinarySearch(low, m-1, key)
+		return s.doBinarySearch(low, m-1, key)
 	} else {
-		return s.BinarySearch(m+1, high, key)
+		return s.doBinarySearch(m+1, high, key)
 	}
 
 }
 
+// binarySearch handles calling the string-specific BinarySearch with args
+func (s SearchString) binarySearch() int {
+	return s.doBinarySearch(0, len(s.List)-1, s.Key)
+}
+
 // BinarySearch implements the binary search algorithm on an array of strings
 // assuming that the input array is already sorted
-func (s SearchString) BinarySearch(low int, high int, key string) int {
+func (s SearchString) doBinarySearch(low int, high int, key string) int {
 
 	m := mid(low, high)
 	if m < 0 {
@@ -43,9 +65,9 @@ func (s SearchString) BinarySearch(low int, high int, key string) int {
 	}
 
 	if key < s.List[m] {
-		return s.BinarySearch(low, m-1, key)
+		return s.doBinarySearch(low, m-1, key)
 	} else {
-		return s.BinarySearch(m+1, high, key)
+		return s.doBinarySearch(m+1, high, key)
 	}
 
 }
--- binarysearch_test.go
+++ binarysearch_test.go
@@ -6,35 +6,34 @@ import (
 
 type inttestgroup struct {
 	input SearchInt
-	key   int
-	index int
+	index int // expected index returned
 }
 
 type stringtestgroup struct {
 	input SearchString
-	key   string
-	index int
+	index int // expected index returned
 }
 
 func testGroupSetupInt(t *testing.T) []inttestgroup {
 	tests := []inttestgroup{
-		{SearchInt{[]int{}}, 3, -1},
-		{SearchInt{[]int{1, 2, 3, 4}}, 3, 2},
-		{SearchInt{[]int{1, 2, 3, 4}}, 7, -1},
-		{SearchInt{[]int{8, 9, 10, 11}}, 7, -1},
-		{SearchInt{[]int{8, 10, 9, 11}}, 9, -1}, // incorrectly sorted
-		{SearchInt{[]int{3, 5, 8, 10, 12, 15, 18, 20, 20, 50, 60}}, 50, 9},
-		{SearchInt{[]int{1, 5, 8, 12, 13}}, 8, 2},
-		{SearchInt{[]int{1, 5, 8, 12, 13}}, 1, 0},
-		{SearchInt{[]int{1, 5, 8, 12, 13}}, 23, -1},
-		{SearchInt{[]int{1, 5, 8, 12, 13}}, 1, 0},
-		{SearchInt{[]int{1, 5, 8, 12, 13}}, 11, -1},
+
+		{SearchInt{[]int{}, 3}, -1},
+		{SearchInt{[]int{1, 2, 3, 4}, 3}, 2},
+		{SearchInt{[]int{1, 2, 3, 4}, 7}, -1},
+		{SearchInt{[]int{8, 9, 10, 11}, 7}, -1},
+		{SearchInt{[]int{8, 10, 9, 11}, 9}, -1}, // incorrectly sorted
+		{SearchInt{[]int{3, 5, 8, 10, 12, 15, 18, 20, 20, 50, 60}, 50}, 9},
+		{SearchInt{[]int{1, 5, 8, 12, 13}, 8}, 2},
+		{SearchInt{[]int{1, 5, 8, 12, 13}, 1}, 0},
+		{SearchInt{[]int{1, 5, 8, 12, 13}, 23}, -1},
+		{SearchInt{[]int{1, 5, 8, 12, 13}, 1}, 0},
+		{SearchInt{[]int{1, 5, 8, 12, 13}, 11}, -1},
 	}
 	var bigarray []int
 	for i := 1; i <= 1000; i++ {
 		bigarray = append(bigarray, i)
 	}
-	bigtest := inttestgroup{SearchInt{bigarray}, 150, 149}
+	bigtest := inttestgroup{SearchInt{bigarray, 150}, 149}
 	tests = append(tests, bigtest)
 
 	return tests
@@ -42,7 +41,7 @@ func testGroupSetupInt(t *testing.T) []inttestgroup {
 
 func TestBinarySearchInt(t *testing.T) {
 	for _, group := range testGroupSetupInt(t) {
-		ret := group.input.BinarySearch(0, len(group.input.List)-1, group.key)
+		ret := BinarySearch(group.input)
 		if ret != group.index {
 			t.Error(
 				"For", group.input,
@@ -56,15 +55,15 @@ func TestBinarySearchInt(t *testing.T) {
 
 func testGroupSetupString(t *testing.T) []stringtestgroup {
 	tests := []stringtestgroup{
-		{SearchString{[]string{}}, "blah", -1},
-		{SearchString{[]string{"one", "two", "three", "four"}}, "three", -1},
-		{SearchString{[]string{"one", "three", "two", "four"}}, "four", -1},
-		{SearchString{[]string{"four", "one", "three", "two"}}, "four", 0},
-		{SearchString{[]string{"four", "one", "three", "two"}}, "one", 1},
-		{SearchString{[]string{"four", "one", "three", "two"}}, "three", 2},
-		{SearchString{[]string{"four", "one", "three", "two"}}, "two", 3},
-		{SearchString{[]string{"a", "n", "w", "z"}}, "n", 1},
-		{SearchString{[]string{"a", "n", "w", "z"}}, "z", 3},
+		{SearchString{[]string{}, "blah"}, -1},
+		{SearchString{[]string{"one", "two", "three", "four"}, "three"}, -1},
+		{SearchString{[]string{"one", "three", "two", "four"}, "four"}, -1},
+		{SearchString{[]string{"four", "one", "three", "two"}, "four"}, 0},
+		{SearchString{[]string{"four", "one", "three", "two"}, "one"}, 1},
+		{SearchString{[]string{"four", "one", "three", "two"}, "three"}, 2},
+		{SearchString{[]string{"four", "one", "three", "two"}, "two"}, 3},
+		{SearchString{[]string{"a", "n", "w", "z"}, "n"}, 1},
+		{SearchString{[]string{"a", "n", "w", "z"}, "z"}, 3},
 	}
 
 	return tests
@@ -72,7 +71,7 @@ func testGroupSetupString(t *testing.T) []stringtestgroup {
 
 func TestBinarySearchString(t *testing.T) {
 	for _, group := range testGroupSetupString(t) {
-		ret := group.input.BinarySearch(0, len(group.input.List)-1, group.key)
+		ret := BinarySearch(group.input)
 		if ret != group.index {
 			t.Error(
 				"For", group.input,
